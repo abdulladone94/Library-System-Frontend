df16d944d3e9d96295cbb5cdb2ff23ed1203b75e
abdulladone94 <you@example.com>
2022-02-21 06:05:50 +0530

    Created Login and Sign up components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hellow World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
